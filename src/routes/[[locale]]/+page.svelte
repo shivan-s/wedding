6f06a8c56c1b2ad074d615022b7874091e46ec10
Shivan Sivakumaran <me@shivan.xyz>
2026-01-29 22:58:57 +1300

    chore: update

---
+++ src/routes/[[locale]]/+page.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+	import Card from './Card.svelte';
+	import ScrollDown from './ScrollDown.svelte';
+	import { m } from '$i18n/messages.js';
+	import favicon from '$lib/assets/favicon.svg';
+	import '$lib/assets/css/app.css';
+	import Ganesh from './Ganesh.svelte';
+	import Lang from './Lang.svelte';
+</script>
+
+<svelte:head>
+	<title>{m.wedding()}</title>
+	<link rel="icon" href={favicon} />
+	<!-- TODO update the font -->
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Baloo+Thambi+2:wght@400..800&family=Courgette&family=Imperial+Script&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
+<main>
+	<header>
+		<Ganesh />
+		<h1 id="top">X weds Y</h1>
+		<Lang />
+		<ScrollDown />
+	</header>
+	<Card />
+	<footer>Thank you</footer>
+</main>
+
+<style>
+	main {
+		& > header,
+		& > footer {
+			display: grid;
+			gap: 1rem;
+			min-height: 100dvh;
+			place-content: center;
+			text-align: center;
+			scroll-snap-align: start;
+		}
+	}
+
+	.imperial {
+		font-family: 'Imperial Script', cursive;
+		font-weight: 400;
+		font-style: normal;
+	}
+
+	.ephesis {
+		font-family: 'Ephesis', cursive;
+		font-weight: 400;
+		font-style: normal;
+	}
+
+	main {
+		font-family: 'Courgette', cursive;
+		font-weight: 400;
+		font-style: normal;
+	}
+</style>
---
+++ src/routes/[[locale]]/Card.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+	import { m } from '$i18n/messages.js';
+	import { getLocale } from '$i18n/runtime';
+</script>
+
+<section>
+	<header>
+		<p>{@html m.aum_sivamayam()}</p>
+		<h2>{@html m.wedding_invitation()}</h2>
+	</header>
+	<p>
+		{@html m.paragraph_1()}
+	</p>
+	<div>
+		<p>
+			{@html m.son()}
+			<br />
+			<span>{m.shivan()}</span>
+		</p>
+		<p>
+			{@html m.daughter()}
+			<br />
+			<span>{m.kasthoori()}</span>
+		</p>
+	</div>
+	<p>
+		{@html m.paragraph_2()}
+	</p>
+	{#if getLocale() === 'ta-lk'}
+		<p>{m.tamil_venue()}</p>
+		<p>{m.tamil_address()}</p>
+		<p>{m.tamil_parents()}</p>
+	{/if}
+	<p>
+		{m.invitation_only()}
+	</p>
+	{#if getLocale() === 'en-au'}
+		<p>
+			{m.rsvp_only()}
+		</p>
+	{/if}
+</section>
+
+<style>
+	section {
+		display: grid;
+		align-content: start;
+		gap: var(--gap);
+		justify-content: center;
+		min-height: calc(100dvh - 2rem);
+		margin: 1rem;
+		padding: 1rem;
+		border: 6px var(--color) double;
+		scroll-snap-align: start;
+		& header {
+			text-align: center;
+		}
+		& > * {
+			max-width: 60ch;
+		}
+		& > div {
+			display: grid;
+			grid-template-columns: repeat(2, 1fr);
+			text-align: center;
+			p > span {
+				font-family: 'Imperial Script', cursive;
+				font-weight: 900;
+				font-style: normal;
+				text-shadow: 0rem 0.1rem 0.1rem hsl(from var(--color) h s l / 60%);
+			}
+		}
+	}
+</style>
