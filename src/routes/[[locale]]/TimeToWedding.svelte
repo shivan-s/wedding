1511d21813a3ba8ff5572f3aeb7d8c525b53a205
Shivan Sivakumaran <me@shivan.xyz>
2026-01-30 23:34:12 +1300

    chore: add times

--- src/routes/[[locale]]/TimeToWedding.svelte
+++ src/routes/[[locale]]/TimeToWedding.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+	import { getLocale } from '$i18n/runtime';
 	import { formatDistanceToNowStrict } from 'date-fns';
 	const weddingDate = new Date('2026-09-13T11:00:00+10:00');
 
 	let d = $derived(formatDistanceToNowStrict(weddingDate, { addSuffix: true, unit: 'day' }));
 </script>
 
-<time datetime={weddingDate.toISOString()}>{d}</time>
+<time datetime={weddingDate.toISOString()}>
+	{weddingDate.toLocaleDateString(getLocale(), { dateStyle: 'full' })} ({d})
+</time>
