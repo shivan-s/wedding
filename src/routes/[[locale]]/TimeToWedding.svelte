e2d03d6240eca628d1bda132d63bc886437fb54a
Shivan Sivakumaran <me@shivan.xyz>
2026-01-30 23:27:34 +1300

    chore: added improvements

---
+++ src/routes/[[locale]]/TimeToWedding.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { formatDistanceToNowStrict } from 'date-fns';
+	const weddingDate = new Date('2026-09-13T11:00:00+10:00');
+
+	let d = $derived(formatDistanceToNowStrict(weddingDate, { addSuffix: true, unit: 'day' }));
+</script>
+
+<time datetime={weddingDate.toISOString()}>{d}</time>
