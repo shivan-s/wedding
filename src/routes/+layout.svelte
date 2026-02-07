001b60a8e48f6b33fcf2537b2084ac0046e681f6
Shivan Sivakumaran <me@shivan.xyz>
2026-02-07 21:34:18 +1300

    feat: add loaders

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,25 @@
 <script lang="ts">
+	import { PUBLIC_IS_PREVIEW } from '$env/static/public';
 	import type { LayoutProps } from './$types';
 	import { m } from '$i18n/messages.js';
 	import favicon from '$lib/assets/favicon.svg';
+
+	const isPreview = PUBLIC_IS_PREVIEW === '1';
 
 	let { children }: LayoutProps = $props();
 </script>
 
 <svelte:head>
-	<title>{m.wedding()}</title>
+	{#if isPreview}
+		<title>{m.wedding()}</title>
+	{:else}
+		<title>Coming soon...</title>
+	{/if}
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+{#if isPreview}
+	{@render children()}
+{:else}
+	Coming soon...
+{/if}
